7e208410742d12c942ed8d914675317ddbd261d2
Tosaaaki <www.tosakiweb.net@gmail.com>
2025-05-23 13:40:11 +0900

    fix admin import for ESM

--- functions/src/admin.ts
+++ functions/src/admin.ts
@@ -1,10 +1,11 @@
 import * as firebaseAdmin from 'firebase-admin';
 
 /** Firebase Admin singleton (Node 20 + ESM) */
-const admin = firebaseAdmin as unknown as typeof firebaseAdmin;
+const admin = (firebaseAdmin as any).default ?? firebaseAdmin;
 
 if (!admin.apps.length) {
   admin.initializeApp();
 }
 
 export default admin;
+
