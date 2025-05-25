c82f2ec75a316544feb50f4d0b7c02fa284ed19a
Tosaaaki <www.tosakiweb.net@gmail.com>
2025-05-25 22:40:44 +0900

    feat: add data list and update tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,22 @@ import { Routes, Route, Link, Navigate } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Review from './pages/Review';
 import Stats from './pages/Stats';
+import DataList from './pages/DataList';
 
 const App: React.FC = () => (
   <div>
     <nav>
       <Link to="/signup">Signup</Link> |{' '}
       <Link to="/review">Review</Link> |{' '}
-      <Link to="/stats">Stats</Link>
+      <Link to="/stats">Stats</Link> |{' '}
+      <Link to="/data">Data</Link>
     </nav>
     <Routes>
       <Route path="/" element={<Navigate to="/signup" replace />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/review" element={<Review />} />
       <Route path="/stats" element={<Stats />} />
+      <Route path="/data" element={<DataList />} />
     </Routes>
   </div>
 );
